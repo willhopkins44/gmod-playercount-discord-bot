1191684163be4b0152bca0ea86530050408650a7
Will Hopkins <willhopkins44@gmail.com>
2022-01-24 22:51:18 -0500

    Finalized bot

--- bot.js
+++ bot.js
@@ -3,13 +3,34 @@ const Gamedig = require('gamedig');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.on('ready', () => {
-    // get target channels
-    // set target channel as number of players and IP
+client.on('ready', async () => {
+    const serverIP = process.env.SERVER_IP;
 
-    // set status as playing on server
+    const guild = client.guilds.cache.get(process.env.DISCORD_GUILD_ID);
+    const playerChannel = guild.channels.cache.get(process.env.PLAYERCOUNT_CHANNEL_ID);
+    const ipChannel = guild.channels.cache.get(process.env.IP_CHANNEL_ID);
 
-    // add steam connect link to profile
+    setInterval(async () => {
+        let serverPing;
+        try {
+            serverPing = await Gamedig.query({
+                type: "garrysmod",
+                host: serverIP.split(":")[0],
+                port: serverIP.split(":")[1]
+            });
+        }
+        catch (ex) {
+            console.log(ex);
+        }
+    
+        const playerCount = serverPing.players.length;
+        const maxPlayers = serverPing.maxplayers;
+    
+        playerChannel.setName(`${playerCount}/${maxPlayers} players online`)
+        ipChannel.setName(process.env.SERVER_IP);
+    
+        client.user.setActivity(` on AlienRP - ${playerCount}/${maxPlayers}`)
+    }, 5000);
 
     console.log("Bot initialized");
 })
