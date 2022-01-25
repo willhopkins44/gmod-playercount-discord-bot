be8afa0441d6b67eb6d217788da633981aaf5920
Will Hopkins <willhopkins44@gmail.com>
2022-01-24 21:35:56 -0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,15 @@
+const { Client, Intents } = require('discord.js');
+const Gamedig = require('gamedig');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+    // get target channels
+    // set target channel as number of players and IP
+
+    // set status as playing on server
+
+    // add steam connect link to profile
+})
+
+client.login(token);
