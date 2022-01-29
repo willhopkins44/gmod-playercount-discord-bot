c914e357e99d5be035852b8f7c8db3b085d6e9f6
Will Hopkins <willhopkins44@gmail.com>
2022-01-29 15:49:50 -0500

    Adjusted try catch block to cover entire operation, configuring ping interval from launch settings

--- bot.js
+++ bot.js
@@ -9,28 +9,28 @@ client.on('ready', async () => {
     const guild = client.guilds.cache.get(process.env.DISCORD_GUILD_ID);
     const playerChannel = guild.channels.cache.get(process.env.PLAYERCOUNT_CHANNEL_ID);
     const ipChannel = guild.channels.cache.get(process.env.IP_CHANNEL_ID);
+    const pingInterval = process.env.PING_INTERVAL;
 
     setInterval(async () => {
-        let serverPing;
         try {
-            serverPing = await Gamedig.query({
+            const serverPing = await Gamedig.query({
                 type: "garrysmod",
                 host: serverIP.split(":")[0],
                 port: serverIP.split(":")[1]
             });
+    
+            const playerCount = serverPing.players.length;
+            const maxPlayers = serverPing.maxplayers;
+        
+            playerChannel.setName(`${playerCount}/${maxPlayers} players online`)
+            ipChannel.setName(process.env.SERVER_IP);
+        
+            client.user.setActivity(`on AlienRP - ${playerCount}/${maxPlayers}`)
         }
         catch (ex) {
             console.log(ex);
         }
-    
-        const playerCount = serverPing.players.length;
-        const maxPlayers = serverPing.maxplayers;
-    
-        playerChannel.setName(`${playerCount}/${maxPlayers} players online`)
-        ipChannel.setName(process.env.SERVER_IP);
-    
-        client.user.setActivity(` on AlienRP - ${playerCount}/${maxPlayers}`)
-    }, 5000);
+    }, pingInterval);
 
     console.log("Bot initialized");
 })
